5c449bd4b61b08fb2ace31f4b42710c5f305ca7f
MsMatias <matias.miranda.ms@gmail.com>
2019-06-28 20:30:35 -0300

    outputs samples simulations

--- PRUEBA.py
+++ PRUEBA.py
@@ -4,17 +4,23 @@
 import pandas as pd
 import numpy as np
 import os
+import sys
 from Mixture import Mixture
 from multiprocessing import Pool
 from IPython.core.interactiveshell import InteractiveShell
 InteractiveShell.ast_node_interactivity = "all"
 
+if len(sys.argv) < 2:
+	print('Write first the dataset and output name file')
+	print('Example: TCGA output_TCGA')
+	sys.exit()
+
 # Read xlsx files
 X = pd.read_excel('LM22Signature.xlsx', sheet_name = 0)
-Y = pd.read_excel('BRCA.TCGA.xlsx', sheet_name = 0)
+Y = pd.read_excel(sys.argv[1] + '.xlsx', sheet_name = 0)
 
 # Run Mixer Function
-data = Mixture(X, Y , 1, 2, nameFile = 'Salida_Cancer')
+data = Mixture(X, Y , 1, 2, nameFile = sys.argv[2])
 
 
 data
