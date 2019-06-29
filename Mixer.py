6558260fe7311ef68aa32671978d4fede2590e73
Matias Miranda <matias.miranda.ms@gmail.com>
2019-06-29 20:56:57 -0300

    sample test #2

--- Mixer.py
+++ Mixer.py
@@ -58,7 +58,7 @@ def Mixer(X, Y, cores):
     if __name__ == 'Mixer':
         for i, j in Yn.iteritems():
             recv_end, send_end = Pipe(False)
-            p = Process(target=nuSvmRobust, args=(X, j, i, [0.25, 0.5, 0.75], 0.007, 6, 1, send_end = ''))
+            p = Process(target=nuSvmRobust, args=(X, j, i, [0.25, 0.5, 0.75], 0.007, 6, 1, send_end))
             processes.append(p)
             p.start()
             pipe_list.append(recv_end)
