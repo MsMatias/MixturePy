dd5ac4283e0f19177ca95082e3de4ea58889e492
Matias Samuel Miranda <matias.miranda.ms@gmail.com>
2019-08-05 15:33:25 -0300

    branch pool

--- Mixture/Mixture/Utils.py
+++ Mixture/Mixture/Utils.py
@@ -13,7 +13,9 @@ def sampleRandom (Y, i, verbose = 0):
     Y = Y.iloc[:, 1:]
     vector = Y.to_numpy(copy=True)    
 
-    return vector[[random.randrange(Y.shape[0] * Y.shape[1]) for x in range(Y.shape[0])]]
+    print(vector)
+
+    return vector
 
 def getPValues (x, i):
     return pd.DataFrame([sum(i.loc[:,'RMSEa'] < x.RMSEa),
