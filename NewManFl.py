05b7e6af67daf9bab434147c4b69a3eed8d43fda
Matias Samuel Miranda <matias.miranda.ms@gmail.com>
2019-10-27 15:55:29 -0300

    Create NewManFl.py

---
+++ NewManFl.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import Mixture
+import Mixture.Utils as mut
+
+# Load Data Signature
+# LM22 OR TIL10
+X = mut.loadSignature('LM22')
+
+# Read xlsx expression file
+Y = pd.read_excel('data/NewmanFL.xlsx', sheet_name = 0) 
+
+# Number of cores that will work
+cores = 4
+
+# Number of permutation samples
+iters = 500
+
+# Name the output file xlsx (without format)
+output = 'Result_Newman'
+
+# Run Mixer Function
+if __name__ == '__main__':
+    result, pValues = Mixture.Mixture(X, Y , cores, iters, output)
