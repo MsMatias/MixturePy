36a0e917a1b29922bb66d7196aaed289b536e329
MsMatias <matias.miranda.ms@gmail.com>
2020-03-24 20:35:04 -0300

    Generating betas

--- testMixture.py
+++ testMixture.py
@@ -5,28 +5,26 @@ import Mixture.Utils as mut
 # Load Data Signature
 # LM22 OR TIL10
 
-X = pd.read_excel('data/TIL9_signature.xlsx', sheet_name = 0)
-X2 = pd.read_excel('data/TIL10_signature.xlsx', sheet_name = 0)
-X3 = pd.read_excel('data/LM22Signature.xlsx', sheet_name = 0)
+X = pd.read_excel('EPICsig_signature.xlsx', sheet_name = 0)
 
 # Read xlsx expression file
-Y = pd.read_excel('data/Celllines.xlsx', sheet_name = 0)
+Y = X #pd.read_excel('EPICsig_betas.xlsx', sheet_name = 0)
+#Y = pd.read_excel('data/Celllines.xlsx', sheet_name = 0)
 
 Y = Y.iloc[: , 1:]
+X = X.iloc[: , 1:]
 
 # Number of cores that will work
 cores = 24
 
 # Number of permutation samples
-iters = 1
+iters = 1000
 
 # Name the output file xlsx (without format)
-output = 'Result_Cellines_TIL9_Mixture'
-output2 = 'Result_Cellines_TIL10_Mixture'
-output3 = 'Result_Cellines_LM22_2_Mixture'
+output = 'Result_Mixture_TIL7_TIL7'
 
 # Run Mixer Function
 if __name__ == '__main__':
 #    result, pValues = Mixture.Mixture(X, Y , cores, iters, output)
 #    result, pValues = Mixture.Mixture(X2, Y , cores, iters, output2)
-    result, pValues = Mixture.Mixture(X3, Y , cores, iters, output3)
+    result, pValues = Mixture.Mixture(X, Y , cores, iters, output)
