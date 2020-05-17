8c0c1efbf9290200bb7a3ab4d7319abe13491ee9
MsMatias <matias.miranda.ms@gmail.com>
2020-05-17 11:52:19 -0300

    test emixture

--- testMixture.py
+++ testMixture.py
@@ -5,15 +5,18 @@ import Mixture.Utils as mut
 # Load Data Signature
 # LM22 OR TIL10
 
-X = pd.read_excel('data/LM22Signature.xlsx', sheet_name = 0)
+X = pd.read_excel('data/TIL9_signature.xlsx', sheet_name = 0)
 
 # Read xlsx expression file
 #Y = X #pd.read_excel('EPICsig_betas.xlsx', sheet_name = 0)
 #Y = pd.read_excel('data/NewmanFL.xlsx', sheet_name = 0)
-Y = pd.read_excel('data/Celllines.xlsx', sheet_name = 0)
+Y = pd.read_excel('data/betas.list.TIL9.xlsx', sheet_name = 0)
 
 Y = Y.iloc[: , 1:]
+Y['Gene symbol'] = X['Gene symbol']
 #X = X.iloc[: , 1:]
+cols = list(Y.columns)
+Y = Y[[cols[-1]] + cols[:-1]]
 
 # Number of cores that will work
 cores = 24
@@ -22,7 +25,7 @@ cores = 24
 iters = 1000
 
 # Name the output file xlsx (without format)
-output = 'Result_Mixture_esvr_celllines_lm22'
+output = 'Result_eMixture_til9'
 
 # Run Mixer Function
 if __name__ == '__main__':
